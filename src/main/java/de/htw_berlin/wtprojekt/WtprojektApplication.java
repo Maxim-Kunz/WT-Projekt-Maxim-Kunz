75abfdd9433809192e65e6d3d6f6109158eb91b2
Maxim.Kunz <maximkunz065@gmail.com>
2026-04-09 19:11:30 +0200

    Commit 1

---
+++ src/main/java/de/htw_berlin/wtprojekt/WtprojektApplication.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.wtprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtprojektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htw_berlin/wtprojekt/WtprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.wtprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
