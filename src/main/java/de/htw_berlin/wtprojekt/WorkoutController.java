e98475220f07862bff320cefcde2d8089ab4485a
Maxim.Kunz <maximkunz065@gmail.com>
2026-05-03 09:25:38 +0200

    M3

--- src/main/java/de/htw_berlin/wtprojekt/WorkoutController.java
+++ src/main/java/de/htw_berlin/wtprojekt/WorkoutController.java
@@ -1,10 +1,12 @@
 package de.htw_berlin.wtprojekt;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class WorkoutController {
 
